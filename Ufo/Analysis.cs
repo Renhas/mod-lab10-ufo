1da3ab83964b317eaa1c42f718e294ea4df869ea
Hasan <emin.oren.139@gmail.com>
2023-05-28 15:42:50 +0300

    Change analysis

--- Ufo/Analysis.cs
+++ Ufo/Analysis.cs
@@ -11,34 +11,26 @@ namespace Ufo
     {
         static public readonly Semaphore semaphore = new Semaphore(0, 1);
         static readonly double distance = 1e5;
-        //static readonly double min_distance = 1e-3;
-        //static readonly double max_distance = 1;
-        static readonly int max_acc = (int)5e2;
-        //static readonly int count_of_points = (int)1e2;
+        static readonly double min_distance = 1e-3;
+        static readonly double max_distance = 1e-1;
+        static readonly int max_acc = (int)1e4;
+        static readonly int count_of_points = (int)1e2;
         static readonly double step = 1;
         
         static private int count = 0;
         static public void Analyse()
         {
-            double[] acc = new double[max_acc];
-            double[] result = new double[max_acc];
-            double[] x = Linspace(1, 50, 50);
-            double[] arctg = new double[50];
-            for (int i = 0; i < 50; i++) 
-            {
-                arctg[i] = MyMath.Atn(Math.Sqrt(3), (int)x[i]);
-            }
-            Draw(x, arctg);
-            return;
+            //double[] acc = new double[max_acc];
+            //double[] result = new double[max_acc];
 
-            //double[] acc = new double[count_of_points];
-            //double[] distances = Linspace(min_distance, max_distance, count_of_points);
+            double[] acc = new double[count_of_points];
+            double[] distances = Linspace(min_distance, max_distance, count_of_points);
             count = 0;
-            /*
+
             foreach (var dist in distances) 
             {
                 
-                int good_acc = 0;
+                int good_acc = -10;
                 int start = 1;
                 int end = max_acc;
                 int current_acc = (start + end) / 2;
@@ -59,7 +51,7 @@ namespace Ufo
                 count++;
             }
             Draw(distances, acc);
-            */
+            /*
             for (int i = 0; i < max_acc; i++) 
             {
                 acc[i] = i + 1;
@@ -67,7 +59,7 @@ namespace Ufo
                 count = i + 1;
             }
             
-            Draw(acc, result);
+            Draw(acc, result);*/
             
             semaphore.Release();
         }
@@ -99,34 +91,8 @@ namespace Ufo
 
         static private bool IsEnter(int accuracy, double value) 
         {
-            PointD startPoint = new PointD(0, 0);
-            PointD endPoint = new PointD(distance / Math.Sqrt(2), distance / Math.Sqrt(2));
-            PointD currentPoint = (PointD)startPoint.Clone();
-
-            double x1 = startPoint.X, x2 = endPoint.X;
-            double y1 = startPoint.Y, y2 = endPoint.Y;
-            double angle = MyMath.Atn(Math.Abs(y2 - y1) / Math.Abs(x2 - x1), accuracy);
-            
-            double distance_to_end = Distance(currentPoint, endPoint);
-
-            double current_x = currentPoint.X, current_y = currentPoint.Y;
-            double current_distance = distance_to_end;
-            while (distance_to_end > value)
-            {
-                current_x += step * MyMath.Cos(angle, accuracy);
-                current_y += step * MyMath.Sin(angle, accuracy);
-
-                currentPoint = new PointD(current_x, current_y);
-                distance_to_end = Distance(currentPoint, endPoint);
-
-                if (current_distance < distance_to_end) 
-                {
-                    return false;
-                }
-                current_distance = distance_to_end;
-                
-            }
-            return true;
+            double error = AnalyseOne(accuracy);
+            return error / distance < value;
         }
 
         static public double Distance(PointD first, PointD second)
@@ -138,17 +104,22 @@ namespace Ufo
         {
             var plt = new Plot(1920, 1080);
             plt.AddScatter(x, y, markerSize: 0);
+            /*
             plt.Title("Зависимость отклонения от точности");
             plt.XLabel("Точность");
             plt.YLabel("Отклонение");
+            */
+            plt.Title("Зависимость количества членов ряда от радиуса");
+            plt.XLabel("Относительная погрешность");
+            plt.YLabel("Количество членов ряда");
             plt.Grid(false);
             plt.SaveFig("dia.png");
         }
 
         static public float GetProgress() 
         {
-            //return (float)count / count_of_points;
-            return (float)count / max_acc;
+            return (float)count / count_of_points;
+            //return (float)count / max_acc;
         }
 
         public static double[] Linspace(double start_val, double end_val, int steps)
